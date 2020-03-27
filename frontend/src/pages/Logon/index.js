b70477f32e236430ab93c9c2a891c403812a8f7e
Yuri Assayag <yfa@icomp.ufam.edu.br>
2020-03-27 00:14:04 -0400

    Front-end do app

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -8,14 +9,37 @@ import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 export default function Logon(){
+
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+        
+        try{
+            const response = await api.post('sessions', {id});
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+            history.push('/profile');
+        }catch{
+            alert('Falha no Login, tente novamente');
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero"/>
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu login</h1>
-                    <input placeholder="Sua ID"/>
+
+                    <input 
+                    placeholder="Sua ID" 
+                    value={id} 
+                    onChange={e => setId(e.target.value)}
+                    />
+
                     <button className="button" type="submit">Entrar</button>
 
                     <Link className="back-link" to="/register">
