b884bc24f624873beef66d70b7403d2886a6bc41
Yuri Assayag <yfa@icomp.ufam.edu.br>
2020-03-29 02:33:57 -0400

    configs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -23,11 +23,24 @@ routes.post('/ongs', celebrate({
 }) ,OngController.create);
 //routes.delete('/ongs/:id', OngController.delete);
 
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}) ,ProfileController.index);
+
 routes.post('/incidents', IncidentsController.create);
-routes.get('/incidents', IncidentsController.index);
-routes.delete('/incidents/:id', IncidentsController.delete);
 
-routes.get('/profile', ProfileController.index);
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}) ,IncidentsController.index);
 
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}) ,IncidentsController.delete);
 
 module.exports = routes;
